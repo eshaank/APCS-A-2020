00f1bff742b4ca6e1d8ea9662b472a12cd90948a
eshaank <eshaank@gmail.com>
2020-02-13 10:42:52 -0800

    FInished perfect lab

--- APCS-A-2020/src/Unit6/Perfect.java
+++ APCS-A-2020/src/Unit6/Perfect.java
@@ -1,23 +1,48 @@
 package Unit6;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
 import static java.lang.System.*;
 
-public class Perfect
-{
-   private int number;
+public class Perfect {
+	private int number;
 
-	//add constructors
+	// add constructors
+	public Perfect() {
+		number = 10;
+	}
+	public Perfect(int num) {
+		number = num;
+	}
 
-	//add a set method
+	// add a set method
+	void setNumber(int num) {
+		number = num;
+	}
 
-	public boolean isPerfect()
-	{
+	public boolean isPerfect() {
+		int count = 2;
+		int sum = 1;
+		do {
+			if(number % count == 0) {
+				sum += count;
+				count ++;
+			}
+		} while(count < number);
+		
+		if(sum == number) {
+			return true;
+		}
 		return false;
 	}
 
-	//add a toString	
-	
+	// add a toString
+	public String toString() {
+		if(isPerfect()) {
+			return number + " is perfect";
+		}
+		return number + " is not perfect";
+	}
 }
--- APCS-A-2020/src/Unit6/PerfectRunner.java
+++ APCS-A-2020/src/Unit6/PerfectRunner.java
@@ -10,6 +10,50 @@ public class PerfectRunner
 	public static void main( String args[] )
 	{
 		//add test cases
-																
+		
+		//496
+		//45
+		//6
+		//14
+		//8128
+		//1245
+		//33
+		//28
+		//27
+		//33550336
+					
+		Perfect p = new Perfect(10);
+		
+		p.setNumber(496);
+		System.out.println(p);
+		
+		p.setNumber(45);
+		System.out.println(p);
+		
+		p.setNumber(6);
+		System.out.println(p);
+		
+		p.setNumber(14);
+		System.out.println(p);
+		
+		p.setNumber(8128);
+		System.out.println(p);
+		
+		p.setNumber(1245);
+		System.out.println(p);
+		
+		p.setNumber(33);
+		System.out.println(p);
+		
+		p.setNumber(28);
+		System.out.println(p);
+		
+		p.setNumber(27);
+		System.out.println(p);
+		
+		p.setNumber(33550336);
+		System.out.println(p);
+		
+		
 	}
 }
