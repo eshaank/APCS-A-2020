54878f867ea47889a723220b0c719c176d52c846
eshaank <eshaank@gmail.com>
2020-02-13 09:04:25 -0800

    Finished vowels lab

--- APCS-A-2020/src/Unit5/FirstLastVowel.java
+++ APCS-A-2020/src/Unit5/FirstLastVowel.java
@@ -9,6 +9,17 @@ public class FirstLastVowel
 {
    public static String go( String a )
 	{
-		return "add code here";
+	   String vowels = "AEIOUaeiou";
+	   
+	   for (int i = 0; i < vowels.length(); i++) {
+		   if(a.charAt(0) == vowels.charAt(i)|| a.charAt(a.length() - 1) == vowels.charAt(i)) {
+			   return "yes";
+		   }
+	}
+	   
+	   
+	   
+	  
+		return "no";
 	}
 }
