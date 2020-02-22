5733e6c80a4dbdf2d09d08c54579d7bda96390c6
eshaank <eshaank@gmail.com>
2020-02-22 15:46:40 -0800

    finished triangle five

--- APCS-A-2020/src/Unit7/TriangleFive.java
+++ APCS-A-2020/src/Unit7/TriangleFive.java
@@ -1,34 +1,58 @@
 package Unit7;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
 import static java.lang.System.*;
 
-public class TriangleFive
-{
-   private char letter;
-   private int amount;
+public class TriangleFive {
+	private char letter;
+	private int amount;
 
-	public TriangleFive()
-	{
+	public TriangleFive() {
+		letter = 'B';
+		amount = 5;
 	}
 
-	public TriangleFive(char c, int amt)
-	{
+	public TriangleFive(char c, int amt) {
+		setLetter(c);
+		setAmount(amt);
 	}
 
-	public void setLetter(char c)
-	{
+	public void setLetter(char c) {
+		letter = c;
 	}
 
-	public void setAmount(int amt)
-	{
+	public void setAmount(int amt) {
+		amount = amt;
 	}
 
-	public String toString()
-	{
-		String output="";
+	public String toString() {
+		String output = "";
+		char holder = letter;
+		int amount2 = amount;
+		int amount3 = amount;
+
+		for (int i = 0; i < amount; i++) {
+			amount2 = amount;
+			holder = letter;
+			for (int x = 0; x < amount3; x++) {
+				for (int a = 0; a < amount2; a++) {
+					output = output + holder;
+				}
+				amount2 -= 1;
+				if (holder == 'Z') {
+					holder = 'A';
+				} else {
+					holder = (char) (holder + 1);
+				}
+				output = output + " ";
+			}
+			amount3 -= 1;
+			output = output + "\n";
+		}
+
 		return output;
 	}
 }
--- APCS-A-2020/src/Unit7/TriangleFiveRunner.java
+++ APCS-A-2020/src/Unit7/TriangleFiveRunner.java
@@ -1,13 +1,31 @@
 package Unit7;
 //(c) A+ Computer Science
+
 //www.apluscompsci.com
 //Name -
 
 import static java.lang.System.*;
 
-public class TriangleFiveRunner
-{
-   public static void main(String args[])
-   {
+public class TriangleFiveRunner {
+	public static void main(String args[]) {
+		TriangleFive test = new TriangleFive();
+		
+		System.out.println(test);
+		
+		
+		TriangleFive tri = new TriangleFive('C',4);
+		out.println(tri);
+		tri.setLetter('A');
+		tri.setAmount(5);
+		out.println(tri);
+		tri.setLetter('B');
+		tri.setAmount(7);
+		out.println(tri);
+		tri.setLetter('X');
+		tri.setAmount(6);
+		out.println(tri);
+		tri.setLetter('Z');
+		tri.setAmount(8);
+		out.println(tri);
 	}
 }
