77b5d9248d963b4f68c099e69cfc8546a90680b2
eshaank <eshaank@gmail.com>
2020-02-22 15:18:42 -0800

    Finished RPS

--- APCS-A-2020/src/Unit7/RPSRunner.java
+++ APCS-A-2020/src/Unit7/RPSRunner.java
@@ -1,29 +1,48 @@
 package Unit7;
 //(c) A+ Computer Science
+
 // www.apluscompsci.com
 //Name -  
 
 import java.util.Scanner;
 import static java.lang.System.*;
 
-public class RPSRunner
-{
-	public static void main(String args[])
-	{
+public class RPSRunner {
+	public static void main(String args[]) {
 		Scanner keyboard = new Scanner(System.in);
 		char response;
+		String player = "";
+
+		out.print("Rock-Paper-Scissors - pick your weapon[R,P,S]:: ");
+		player = keyboard.next();
+		RockPaperScissors game = new RockPaperScissors(player);
 		
-		//add in a do while loop after you get the basics up and running
+		game.setPlayers(player);
+		System.out.println(game);
+
+		// add in a do while loop after you get the basics up and running
+
+		do {
+			System.out.println("Do you want to play again");
+			response = keyboard.next().charAt(0);
+			out.print("Rock-Paper-Scissors - pick your weapon[R,P,S]:: ");
+			player = keyboard.next();
+			game.setPlayers(player);
+			System.out.println(game);
+		} while (response == 'y');
 		
-			String player = "";
-		
-			out.print("type in your prompt [R,P,S] :: ");
-			
-			//read in the player value
-		
-			RockPaperScissors game = new RockPaperScissors();		
+		System.out.println(game);
+		game.setPlayers("R");
+		System.out.println(game);
+		game.setPlayers("R");
+		System.out.println(game);
+		game.setPlayers("S");
+		System.out.println(game);
+		game.setPlayers("S");
+		System.out.println(game);
+		game.setPlayers("P");
+		System.out.println(game);
+		game.setPlayers("P");
+		System.out.println(game);
 	}
 }
-
-
-
--- APCS-A-2020/src/Unit7/RockPaperScissors.java
+++ APCS-A-2020/src/Unit7/RockPaperScissors.java
@@ -1,37 +1,74 @@
 package Unit7;
 //(c) A+ Computer Science
+
 // www.apluscompsci.com
 //Name -  
 
 import java.util.Scanner;
 import static java.lang.System.*;
 
-public class RockPaperScissors
-{
+public class RockPaperScissors {
 	private String playChoice;
 	private String compChoice;
 
-	public RockPaperScissors()
-	{
+	public RockPaperScissors() {
 	}
 
-	public RockPaperScissors(String player)
-	{
+	public RockPaperScissors(String player) {
+		playChoice = player;
 	}
 
-	public void setPlayers(String player)
-	{
+	public void setPlayers(String player) {
+		playChoice = player;
+		
+		int computer = (int) (Math.random() * 3);
+
+		if (computer == 0) {
+			compChoice = "R";
+		} 
+		else if (computer == 1) {
+			compChoice = "P";
+		} 
+		else {
+			compChoice = "S";
+		}
 	}
 
-	public String determineWinner()
-	{
-		String winner="";
+	public String determineWinner() {
+		String winner = "";
+	
+		
+		
+		if(compChoice.equals(playChoice)) {
+			winner = "!Draw Game!";
+		}
+		else if(compChoice.equals("R") && playChoice.equals("S")) {
+			winner = "!Computer wins <<Rock Beaks Scissors>>!";
+		}
+		else if(compChoice.equals("P") && playChoice.equals("R")) {
+			winner = "!Computer wins <<Paper Covers Rock>>!";
+		}
+		else if(compChoice.equals("S") && playChoice.equals("P")) {
+			winner = "!Computer wins <<Scissors Cuts Paper>>!";
+		}
+		
+		else if(compChoice.equals("S") && playChoice.equals("R")) {
+			winner = "!Player wins <<Rock Beaks Scissors>>!";
+		}
+		else if(compChoice.equals("R") && playChoice.equals("P")) {
+			winner = "!Player wins <<Paper Covers Rock>>!";
+		}
+		else if(compChoice.equals("P") && playChoice.equals("S")) {
+			winner = "!Player wins <<Scissors Cuts Paper>>!";
+		}
+		
+		
+		
 		return winner;
 	}
 
-	public String toString()
-	{
-		String output="";
+	public String toString() {
+		String output = "Player had " + playChoice + "\nComputer had " + compChoice + "\n" + determineWinner() + "\n";
 		return output;
 	}
 }
