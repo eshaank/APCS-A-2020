927e85e3729d33d40e364fc823a3f107c920bbcd
eshaank <eshaank@gmail.com>
2020-02-24 10:43:04 -0800

    added elevens lab

---
+++ APCS-A-2020/src/ElevensLab/Card.java
@@ -0,0 +1,31 @@
+package ElevensLab;
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+//Name -
+
+public class Card {
+
+	public static final String FACES[] = { "ZERO", "ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT",
+			"NINE", "TEN", "JACK", "QUEEN", "KING" };
+
+	// instance variables
+	// String suit
+	// int face
+	private String suit;
+	private int face;
+
+	// constructors
+	public Card(String s, int f) {
+		// TODO Auto-generated constructor stub
+	}
+
+	// modifiers
+	// set methods
+
+	// accessors
+	// get methods
+
+	// toString
+
+}
