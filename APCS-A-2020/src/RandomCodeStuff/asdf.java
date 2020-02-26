36b1987689d89cf19f2c8670e57d1173c35741c2
eshaank <eshaank@gmail.com>
2020-02-26 09:30:07 -0800

    added a random package

---
+++ APCS-A-2020/src/RandomCodeStuff/asdf.java
@@ -0,0 +1,13 @@
+package RandomCodeStuff;
+
+public class asdf {
+	public static void main(String[] args) {
+		System.out.println(truthTable(false, false));
+	}
+	
+	public static boolean truthTable(boolean e, boolean f) {
+		boolean d;
+		d = !(f || e);
+		return d;
+	}
+}
