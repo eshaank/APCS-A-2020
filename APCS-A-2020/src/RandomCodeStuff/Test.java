d3516dd608ba575fbdc20133cd3e74bce6800feb
eshaank <eshaank@gmail.com>
2020-04-02 16:42:13 -0700

    nvm

---
+++ APCS-A-2020/src/RandomCodeStuff/Test.java
@@ -0,0 +1,22 @@
+package RandomCodeStuff;
+
+public class Test {
+	private String name;
+	private int age;
+
+	public Test(String name, int age) {
+		this.name = name;
+		this.age = age;
+		
+		
+	}
+	 String getName(){
+		return name;
+	}
+	 int getAge() {
+		return age;
+	}
+	 void setAge() {
+		age = 10;
+	}
+}
---
+++ APCS-A-2020/src/RandomCodeStuff/TestRunner.java
@@ -0,0 +1,8 @@
+package RandomCodeStuff;
+
+public class TestRunner {
+	public static void main(String[] args) {
+		Test test = new Test("Bob", 9);
+		System.out.println(test.getName() + ", " + test.getAge());
+	}
+}
